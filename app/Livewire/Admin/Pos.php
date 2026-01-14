1a9f3fb2dac84a45a56d39a1294b0c7f96340ada
Rizki <ardhikaz30@gmail.com>
2026-01-14 09:49:57 +0700

    report monthly

--- app/Livewire/Admin/Pos.php
+++ app/Livewire/Admin/Pos.php
@@ -47,7 +47,6 @@ class Pos extends Component
             $this->cart[$id] = [
                 'id' => $product->id,
                 'name' => $product->name,
-                'price' => $product->price,
                 'image' => $product->image,
                 'quantity' => 1
             ];
